236e9c8c694ad93956ba1c16b40188716d569d32
lucciani <giancarlolucciani@gmail.com>
2021-09-19 12:22:43 -0300

    refatorando serviço e controller estado

--- src/main/java/io/github/lucciani/ava/api/EstadoController.java
+++ src/main/java/io/github/lucciani/ava/api/EstadoController.java
@@ -1,12 +1,10 @@
 package io.github.lucciani.ava.api;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.lucciani.ava.domain.exception.EntidadeEmUsoException;
-import io.github.lucciani.ava.domain.exception.EntidadeNaoEncontradaException;
+import io.github.lucciani.ava.domain.exception.NegocioException;
+import io.github.lucciani.ava.domain.exception.RegiaoNaoEncontradaException;
 import io.github.lucciani.ava.domain.model.Estado;
 import io.github.lucciani.ava.domain.repository.EstadoRepository;
 import io.github.lucciani.ava.domain.service.CadastroEstadoService;
@@ -39,48 +37,38 @@ public class EstadoController {
 	}
 
 	@GetMapping(value = "/{estadoId}")
-	public ResponseEntity<Estado> buscar(@PathVariable Long estadoId) {
-		Optional<Estado> estado = estadoRepository.findById(estadoId);
-
-		if (estado.isPresent()) {
-			return ResponseEntity.ok(estado.get());
-		}
-
-		return ResponseEntity.notFound().build();
+	public Estado buscar(@PathVariable Long estadoId) {
+		return cadastroEstado.buscarSeExistir(estadoId);
 	}
 
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public Estado adicionar(@RequestBody Estado estado) {
-		return cadastroEstado.salvar(estado);
+		try {
+			return cadastroEstado.salvar(estado);
+		} catch (RegiaoNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
 	}
 
 	@PutMapping(value = "/{estadoId}")
-	public ResponseEntity<Estado> atualizar(@PathVariable Long estadoId, @RequestBody Estado estado) {
+	public Estado atualizar(@PathVariable Long estadoId, @RequestBody Estado estado) {
+		Estado estadoAtual = cadastroEstado.buscarSeExistir(estadoId);
 
-		Optional<Estado> estadoAtual = estadoRepository.findById(estadoId);
+		BeanUtils.copyProperties(estado, estadoAtual, "id");
 
-		if (estadoAtual.isPresent()) {
-			BeanUtils.copyProperties(estado, estadoAtual.get(), "id");
-			Estado estadoSalvo = cadastroEstado.salvar(estadoAtual.get());
-			return ResponseEntity.ok(estadoSalvo);
+		try {
+			return cadastroEstado.salvar(estadoAtual);
+		} catch (RegiaoNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage(), e);
 		}
 
-		return ResponseEntity.notFound().build();
 	}
 
 	@DeleteMapping(value = "/{estadoId}")
-	public ResponseEntity<?> remover(@PathVariable Long estadoId) {
-		try {
-			cadastroEstado.remover(estadoId);
-			return ResponseEntity.noContent().build();
-
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-		}
-
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long estadoId) {
+		cadastroEstado.remover(estadoId);
 	}
+
 }
--- src/main/java/io/github/lucciani/ava/domain/service/CadastroEstadoService.java
+++ src/main/java/io/github/lucciani/ava/domain/service/CadastroEstadoService.java
@@ -6,17 +6,29 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import io.github.lucciani.ava.domain.exception.EntidadeEmUsoException;
-import io.github.lucciani.ava.domain.exception.EntidadeNaoEncontradaException;
+import io.github.lucciani.ava.domain.exception.EstadoNaoEncontradaException;
 import io.github.lucciani.ava.domain.model.Estado;
+import io.github.lucciani.ava.domain.model.Regiao;
 import io.github.lucciani.ava.domain.repository.EstadoRepository;
 
 @Service
 public class CadastroEstadoService {
 
+	private static final String MSG_ESTADO_EM_USO 
+			= "Estado de código %d não pode ser removido, pois está em uso.";
+
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CadastroRegiaoService cadastroRegiao;
 
 	public Estado salvar(Estado estado) {
+		Long regiaoId = estado.getRegiao().getId();
+		Regiao regiao = cadastroRegiao.buscarSeExistir(regiaoId);
+		
+		estado.setRegiao(regiao);
+		
 		return estadoRepository.save(estado);
 	}
 
@@ -25,11 +37,15 @@ public class CadastroEstadoService {
 			estadoRepository.deleteById(estadoId);
 
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de estado com o código %d", estadoId));
+			throw new EstadoNaoEncontradaException(estadoId);
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format("Estado de código %d não pode ser removido, pois está em uso.", estadoId));
+					String.format(MSG_ESTADO_EM_USO, estadoId));
 		}
+	}
+	
+	public Estado buscarSeExistir(Long estadoId) {
+		return estadoRepository.findById(estadoId).orElseThrow(
+				() -> new EstadoNaoEncontradaException(estadoId));
 	}
 }
